4ea646a79f3fbd98b506efa9b3c55fce0cd1f46f
danik2515 <zahdanik@gmail.com>
2020-10-12 09:08:38 +0300

    Clear master

--- src/bsu/rfe/java/group9/lab1/Zakharevich/varC6/Consumable.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfe.java.group9.lab1.Zakharevich.varC6;
-
-public interface Consumable {
-    public abstract void consume();
-}
--- src/bsu/rfe/java/group9/lab1/Zakharevich/varC6/Nutritous.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfe.java.group9.lab1.Zakharevich.varC6;
-
-public interface Nutritous {
-        public abstract Double calculateCalories();
-}
