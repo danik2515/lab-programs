b852f23e5208217a4137c313dd411e3ba61972a5
danik2515 <zahdanik@gmail.com>
2021-04-12 23:16:36 +0300

    added menu bar

--- src/bsu/rfe/java/group9/lab7/Zakharevich/varC6/MainFrame.java
+++ src/bsu/rfe/java/group9/lab7/Zakharevich/varC6/MainFrame.java
@@ -1,230 +1,233 @@
 package bsu.rfe.java.group9.lab7.Zakharevich.varC6;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.HierarchyBoundsListener;
+import java.awt.event.HierarchyEvent;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import javax.swing.BorderFactory;
-import javax.swing.GroupLayout;
+import java.util.HashMap;
+import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+
 @SuppressWarnings("serial")
-public class MainFrame extends JFrame {
-    private static final String FRAME_TITLE = "Клиент мгновенных сообщений";
-    private static final int FRAME_MINIMUM_WIDTH = 500;
-    private static final int FRAME_MINIMUM_HEIGHT = 500;
-    private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
-    private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
-    private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
-    private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
-    private static final int SMALL_GAP = 5;
-    private static final int MEDIUM_GAP = 10;
-    private static final int LARGE_GAP = 15;
-    private static final int SERVER_PORT = 4567;
-    private final JTextField textFieldFrom;
-    private final JTextField textFieldTo;
-    private final JTextArea textAreaIncoming;
-    private final JTextArea textAreaOutgoing;
+public class MainFrame extends JFrame{
+    private static String name=null;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JList<String> list;
+    private DefaultListModel<String> listModel;
+    private HashMap<String, JTextArea> map = new HashMap<String, JTextArea>();
+    private HashMap<String, String> map2 = new HashMap<String, String>();
+    private JTextArea forall;
+    private static Integer srvPort;
+    private static Integer id;
+    private static boolean fail=false;
+    private int sMAX_LIST_W=200;
+    private int sTAM_H=70;
+    private JButton btnsall;
+    private static MainFrame THIS;
 
-    public MainFrame() {
-        super(FRAME_TITLE);
-        setMinimumSize(
-                new Dimension(FRAME_MINIMUM_WIDTH, FRAME_MINIMUM_HEIGHT));
 
-// Центрирование окна
-        final Toolkit kit = Toolkit.getDefaultToolkit();
-        setLocation((kit.getScreenSize().width - getWidth()) / 2,
-                (kit.getScreenSize().height - getHeight()) / 2);
-// Текстовая область для отображения полученных сообщений
-        textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
-        textAreaIncoming.setEditable(false);
-// Контейнер, обеспечивающий прокрутку текстовой области
-        final JScrollPane scrollPaneIncoming =
-                new JScrollPane(textAreaIncoming);
-// Подписи полей
-        final JLabel labelFrom = new JLabel("Подпись");
-        final JLabel labelTo = new JLabel("Получатель");
-// Поля ввода имени пользователя и адреса получателя
-        textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
-        textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
-// Текстовая область для ввода сообщения
-        textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
-// Контейнер, обеспечивающий прокрутку текстовой области
-        final JScrollPane scrollPaneOutgoing =
-                new JScrollPane(textAreaOutgoing);
-// Панель ввода сообщения
-        final JPanel messagePanel = new JPanel();
-        messagePanel.setBorder(
-                BorderFactory.createTitledBorder("Сообщение"));
-// Кнопка отправки сообщения
-        final JButton sendButton = new JButton("Отправить");
-        sendButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sendMessage();
+    MainFrame(boolean s) throws IOException{
+        super("Клиент мгновенных сообщений");
+        THIS=this;
+        final JFrame frame=this;
+        setMinimumSize(new Dimension(570,390));
+        setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - getWidth()) / 2
+                , (Toolkit.getDefaultToolkit().getScreenSize().height - getHeight()) / 2);
+        if(s){
+            final JLabel label=new JLabel("Введите имя:");
+            final JTextField textName = new JTextField("",10);
+            textName.setMaximumSize(textName.getPreferredSize());
+
+            final JButton button = new JButton("Подтвердить имя");
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent arg0) {
+                    name=textName.getText();
+                }
+            });
+            Box box0 = Box.createHorizontalBox();
+            box0.add(label,BorderLayout.CENTER);
+            Box box1 = Box.createHorizontalBox();
+            box1.add(textName,BorderLayout.CENTER);
+            Box box2 = Box.createHorizontalBox();
+            box2.add(button,BorderLayout.CENTER);
+            Box box = Box.createVerticalBox();
+            box.add(Box.createVerticalGlue());
+            box.add(box0);
+            box.add(Box.createVerticalStrut(10));
+            box.add(box1);
+            box.add(Box.createVerticalStrut(20));
+            box.add(box2);
+            box.add(Box.createVerticalGlue());
+            getContentPane().add(box,BorderLayout.CENTER);
+            return;
+        }
+        setLayout(null);
+        setTitle("Ваше имя: "+name);
+        id=(int) (Math.random()*1000000000);
+        listModel=new DefaultListModel<String>();
+        listModel.addElement("Общий");
+        list=new JList<String>(listModel);
+        list.setLayoutOrientation(JList.VERTICAL);
+        final JScrollPane scrollPaneList=new JScrollPane(list);
+        list.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent arg0) {
+                if(list.getSelectedIndex()==0){
+                    tabbedPane.setSelectedIndex(0);
+                    return;
+                }
+                if(arg0.getValueIsAdjusting())
+                    return;
+                int i=0;
+                int si=list.getSelectedIndex();
+                if(si<0)
+                    return;
+                String s=listModel.get(si);
+                String[] _a=s.split("@");
+                s=_a[0];
+                while(i<tabbedPane.getTabCount()){
+                    if(tabbedPane.getTitleAt(i).equals(s)){
+                        tabbedPane.setSelectedIndex(i);
+                        return;
+                    }
+                    i++;
+                }
+                String[] a = listModel.get(list.getSelectedIndex()).toString().split("@");
+                newTab(a[0],a[1]);
             }
         });
-// Компоновка элементов панели "Сообщение"
-        final GroupLayout layout2 = new GroupLayout(messagePanel);
-        messagePanel.setLayout(layout2);
-        layout2.setHorizontalGroup(layout2.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout2.createParallelGroup(GroupLayout.Alignment.TRAILING)
-                        .addGroup(layout2.createSequentialGroup()
-                                .addComponent(labelFrom)
-                                .addGap(SMALL_GAP)
-                                .addComponent(textFieldFrom)
-                                .addGap(LARGE_GAP)
-                                .addComponent(labelTo)
-                                .addGap(SMALL_GAP)
-                                .addComponent(textFieldTo))
-                        .addComponent(scrollPaneOutgoing)
-                        .addComponent(sendButton))
-                .addContainerGap());
-        layout2.setVerticalGroup(layout2.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout2.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(labelFrom)
-                        .addComponent(textFieldFrom)
-                        .addComponent(labelTo)
-                        .addComponent(textFieldTo))
-                .addGap(MEDIUM_GAP)
-                .addComponent(scrollPaneOutgoing)
-                .addGap(MEDIUM_GAP)
-                .addComponent(sendButton)
-                .addContainerGap());
-// Компоновка элементов фрейма
-        final GroupLayout layout1 = new GroupLayout(getContentPane());
-        setLayout(layout1);
-        layout1.setHorizontalGroup(layout1.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout1.createParallelGroup()
-                        .addComponent(scrollPaneIncoming)
-                        .addComponent(messagePanel))
-                .addContainerGap());
-        layout1.setVerticalGroup(layout1.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(scrollPaneIncoming)
-                .addGap(MEDIUM_GAP)
-                .addComponent(messagePanel)
-                .addContainerGap());
-// Создание и запуск потока-обработчика запросов
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    final ServerSocket serverSocket =
-                            new ServerSocket(SERVER_PORT);
-                    while (!Thread.interrupted()) {
-                        final Socket socket = serverSocket.accept();
-                        final DataInputStream in = new DataInputStream(
-                                socket.getInputStream());
-// Читаем имя отправителя
-                        final String senderName = in.readUTF();
-// Читаем сообщение
-                        final String message = in.readUTF();
-// Закрываем соединение
-                        socket.close();
-// Выделяем IP-адрес
-                        final String address =
-                                ((InetSocketAddress) socket
-                                        .getRemoteSocketAddress())
-                                        .getAddress()
-                                        .getHostAddress();
-// Выводим сообщение в текстовую область
-                        textAreaIncoming.append(senderName +
-                                " (" + address + "): " +
-                                message + "\n");
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    JOptionPane.showMessageDialog(MainFrame.this,
-                            "Ошибка в работе сервера", "Ошибка",
-                            JOptionPane.ERROR_MESSAGE);
-                }
+        tabbedPane.setLocation(210,3);
+        tabbedPane.setSize(350,360);
+        scrollPaneList.setLocation(3,3);
+        scrollPaneList.setSize(200,360);
+        this.add(scrollPaneList);
+        this.add(tabbedPane);
+        this.getContentPane().addHierarchyBoundsListener(new HierarchyBoundsListener() {
+            public void ancestorResized(HierarchyEvent arg0) {
+                scrollPaneList.setSize(sMAX_LIST_W,frame.getHeight()-30);
+                tabbedPane.setSize(frame.getWidth()-220,frame.getHeight()-30);
             }
-        }).start();
+            public void ancestorMoved(HierarchyEvent arg0) {}
+        });
+
     }
 
-    private void sendMessage() {
-        try {
-// Получаем необходимые параметры
-            final String senderName = textFieldFrom.getText();
-            final String destinationAddress = textFieldTo.getText();
-            final String message = textAreaOutgoing.getText();
-// Убеждаемся, что поля не пустые
-            if (senderName.isEmpty()) {
-                JOptionPane.showMessageDialog(this,
-                        "Введите имя отправителя", "Ошибка",
-                        JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            if (destinationAddress.isEmpty()) {
-                JOptionPane.showMessageDialog(this,
-                        "Введите адрес узла-получателя", "Ошибка",
-                        JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            if (message.isEmpty()) {
-                JOptionPane.showMessageDialog(this,
-                        "Введите текст сообщения", "Ошибка",
-                        JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-// Создаем сокет для соединения
-            final Socket socket =
-                    new Socket(destinationAddress, SERVER_PORT);
-// Открываем поток вывода данных
-            final DataOutputStream out =
-                    new DataOutputStream(socket.getOutputStream());
-// Записываем в поток имя
-            out.writeUTF(senderName);
-// Записываем в поток сообщение
-            out.writeUTF(message);
-// Закрываем сокет
-            socket.close();
-// Помещаем сообщения в текстовую область вывода
-            textAreaIncoming.append("Я -> " + destinationAddress + ": "
-                    + message + "\n");
-// Очищаем текстовую область ввода сообщения
-            textAreaOutgoing.setText("");
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(MainFrame.this,
-                    "Не удалось отправить сообщение: узел-адресат не найден",
+    private void newTab(final String caption, String address){
+        String ip = null;
+        int port = -1;
+        if(address!=null){
+            String[] _a=address.split(":");
+            ip=_a[0];
+            port=Integer.parseInt(_a[1]);
+        }
 
-                    "Ошибка", JOptionPane.ERROR_MESSAGE);
-        } catch (IOException e) {
+        JPanel panel = new JPanel();
+        final JTextArea textAreaHist = new JTextArea(1, 15);
+        textAreaHist.setEditable(false);
+        textAreaHist.addFocusListener(new FocusListener() {
+
+            @Override
+            public void focusLost(FocusEvent arg0) {
+
+
+            }
+
+            @Override
+            public void focusGained(FocusEvent arg0) {
+
+
+            }
+        });
+        final JScrollPane scrollPaneOutgoing = new JScrollPane(textAreaHist);
+        map.put(caption, textAreaHist);
+        final JTextArea textAreaMsg = new JTextArea(1, 15);
+        final JScrollPane scrollPaneMsg = new JScrollPane(textAreaMsg);
+        final String ip2=ip;
+        final int port2=port;
+
+        final JButton btn=new JButton("Отправить");
+
+        if(address==null){
+            btnsall=btn;
+            btn.setEnabled(false);
+
+        }
+        String hist=map2.get(caption);
+        if(hist!=null)
+            textAreaHist.append(hist);
+        tabbedPane.addTab(caption, panel);
+        if(address==null)
+            forall=textAreaHist;
+        panel.setLayout(null);
+        panel.add(btn);
+        btn.setSize(120,20);
+        btn.setLocation(tabbedPane.getWidth()-150,tabbedPane.getHeight()-55);
+        scrollPaneOutgoing.setLocation(10,10);
+        scrollPaneOutgoing.setSize(tabbedPane.getWidth()-20,tabbedPane.getHeight()-150);
+        scrollPaneMsg.setLocation(40,tabbedPane.getHeight()-130);
+        scrollPaneMsg.setSize(tabbedPane.getWidth()-50,sTAM_H);
+        panel.add(scrollPaneOutgoing);
+        panel.add(scrollPaneMsg);
+        panel.addHierarchyBoundsListener(new HierarchyBoundsListener() {
+            public void ancestorResized(HierarchyEvent e) {
+                btn.setLocation(tabbedPane.getWidth()-150,tabbedPane.getHeight()-55);
+                scrollPaneOutgoing.setSize(tabbedPane.getWidth()-20,tabbedPane.getHeight()-150);
+                scrollPaneMsg.setSize(tabbedPane.getWidth()-50,sTAM_H);
+                scrollPaneMsg.setLocation(40,tabbedPane.getHeight()-130);
+            }
+            public void ancestorMoved(HierarchyEvent e) {}
+        });
+
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        try{
+            while(true){
+                MainFrame frame=new MainFrame(true);
+                frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                frame.setVisible(true);
+
+                frame.setVisible(false);
+                Point location = frame.getLocation();
+                Dimension size = frame.getSize();
+
+                frame = new MainFrame(false);
+                frame.setLocation(location);
+                frame.setSize(size);
+                frame.setVisible(true);
+
+                while(!fail){
+                    Thread.sleep(100);
+                }
+                frame.setVisible(false);
+                frame=null;
+                name=null;
+                fail=false;
+            }
+        }catch(IOException | InterruptedException e){
             e.printStackTrace();
-            JOptionPane.showMessageDialog(MainFrame.this,
-                    "Не удалось отправить сообщение", "Ошибка",
-                    JOptionPane.ERROR_MESSAGE);
         }
     }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                final MainFrame frame = new MainFrame();
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setVisible(true);
-            }
-        });
-    }
 }
+
